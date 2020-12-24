76524559430eb04b92e367d7988f05b6ff7931a4
soumyabhaduri <49870333+soumyabhaduri@users.noreply.github.com>
2020-12-24 11:35:00 +0530

    Initial Solution Setup

---
+++ MyShope/MyShope.WebUI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MyShope.WebUI.MvcApplication" Language="C#" %>
---
+++ MyShope/MyShope.WebUI/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MyShope.WebUI.Startup))]
+namespace MyShope.WebUI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
